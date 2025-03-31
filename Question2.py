096d0628f39cb007f8a9afb8fe21e8b358e408e7
besong-hans-ayuk <besonghansayuk@gmail.com>
2025-03-31 09:53:56 +0100

    function that takes a list of numbers and returns a new list containing only even numbers

---
+++ Question2.py
@@ -0,0 +1,13 @@
+#Function that writes a mlist of names 
+
+def write_names_to_file(filename, names):
+    
+    list = ['Alice', 'Bob', 'Charlie' ]
+    
+    names.txt , 
+    
+    write_names_to_file("name.txt",["Alice", "Bob", "Charlie"])
+    
+    print("Alice, Bob, Charlie")
+    
+    
---
+++ Section_1.py
@@ -0,0 +1,15 @@
+#function that takes a list of numbers and returns a new list containing only even numbers
+
+def filter_even_numbers(list):
+    
+   return["num % 2 == 0 , for num is number"]
+
+list= [1, 2, 3, 4, 5, 6]
+
+filter_even_numbers(list)
+   
+filter_even_numbers([1, 2, 3, 4, 5, 6])
+
+print("filter_even_numbers([1, 2, 3, 4, 5, 6])")
+      
+    
